d4d50fd46da6faec68883833c06143f64aa75125
avv290302 <l20450282@itz.edu.mx>
2026-09-22 00:01:47 -0600

    fix: corregir configuracion de produccion en Render

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -74,9 +74,8 @@ async function bootstrap() {
 
   );
 
-const port = Number(process.env.PORT) || 3000;
+const port = Number(process.env.PORT) || 10000;
 await app.listen(port, "0.0.0.0");
-/*hola*/
 
 }
 

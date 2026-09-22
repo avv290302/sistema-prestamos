8a4c010fed6681054c08df9b76ad0ff73dd08b77
avv290302 <l20450282@itz.edu.mx>
2026-09-21 23:41:43 -0600

    fix: escuchar API en todas las interfaces2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -74,7 +74,9 @@ async function bootstrap() {
 
   );
 
- await app.listen(process.env.PORT ?? 3000, "0.0.0.0");
+const port = Number(process.env.PORT) || 3000;
+await app.listen(port, "0.0.0.0");
+/*hola*/
 
 }
 
